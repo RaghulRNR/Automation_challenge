d615e6bcfce07e6ea0e4d3dde7b81780c1ad3d94
Raghul <rahulrnr173@gmail.com>
2022-12-13 16:51:59 +0530

    automation challenge-5

---
+++ Automation_challenge_5.py
@@ -0,0 +1,15 @@
+string1=input('Enter the string1:')
+string2=input('Enter the string2:')
+list1=string1.split()
+list2=string2.split()
+
+reslen=0
+resString=''
+for x in list1:
+    for y in list2:
+        if x.lower()==y.lower():
+            if len(x)>reslen:
+                reslen=len(x)
+                resString=x
+#print(reslen)
+print('Longest shared sub-string is:',resString)
